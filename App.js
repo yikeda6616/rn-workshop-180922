d3aeff6145210211a3a5054e8e39a82f9f22264a
Yasushi Ikeda <yikeda6616@gmail.com>
2019-04-02 15:54:39 -0700

    add primary button

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+  handleOmikuji = () => {
+    alert('daikichi!');
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -11,6 +15,17 @@ export default class App extends React.Component {
           resizeMode='contain'
         />
         <Text style={styles.text}>Today's Fortune?</Text>
+        <Image
+          source={require('./assets/daikichi.png')}
+          style={styles.resultImage}
+          resizeMode='contain'
+        />
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={this.handleOmikuji}
+        >
+          <Text style={styles.buttonTitle}>Draw a fortune</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -29,5 +44,18 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 40
+  },
+  resultImage: {
+    width: 200,
+    height: 300
+  },
+  buttonContainer: {
+    backgroundColor: '#3498db'
+  },
+  buttonTitle: {
+    fontSize: 25,
+    color: 'white',
+    paddingHorizontal: 40,
+    paddingVertical: 10
   }
 });
